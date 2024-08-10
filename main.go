9be85e4ac50652f04411f1a617971f014037e111
Ace0314 <vampirez66@yahoo.com>
2024-08-09 18:27:20 -0600

    added internal api package

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	"github.com/Ace0314/Pokedex-with-Go/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapi: pokeClient,
+	}
+
+	startRepl(cfg)
 }
